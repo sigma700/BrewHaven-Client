095d0aad4c8c358dd188275c5e89e5c2d6ebfa1f
sigma700 <196065961+sigma700@users.noreply.github.com>
2025-10-20 17:49:17 +0300

    added the routes

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Landing from "./pages/Landing";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Landing />,
+  },
+]);
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main>
+      <RouterProvider router={router} />
+    </main>
+  );
 };
 
 export default App;
---
+++ vite-project/src/pages/Landing.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Landing = () => {
+  return <div>landing page for the application</div>;
+};
+
+export default Landing;
